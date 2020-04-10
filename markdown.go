2dd9ae890f7b0236a479653ba0710d3cce9f6780
Alex <alexeek@protonmail.com>
2020-04-10 09:15:43 +0100

    Add point sorting

--- markdown.go
+++ markdown.go
@@ -4,14 +4,33 @@ import (
 	"html/template"
 	"io/ioutil"
 	"path"
+	"sort"
+	"strconv"
 	"strings"
 )
+
+type byFname []string
+
+func (s byFname) Len() int {
+	return len(s)
+}
+
+func (s byFname) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s byFname) Less(i, j int) bool {
+	inds, err := strconv.Atoi(strings.Split(s[i], "_")[0])
+	indj, err := strconv.Atoi(strings.Split(s[j], "_")[0])
+	chk(err)
+	return inds < indj
+}
 
 func getPoints() map[string]template.HTML {
 	dir, err := ioutil.ReadDir("points")
 	chk(err)
 
-	res := map[string]template.HTML{}
+	preSort := map[string]template.HTML{}
 
 	for _, v := range dir {
 		name := v.Name()
@@ -19,8 +38,20 @@ func getPoints() map[string]template.HTML {
 			bytes, err := ioutil.ReadFile(path.Join("points", name))
 			chk(err)
 
-			res[name] = template.HTML(md.RenderToString(bytes))
+			preSort[name] = template.HTML(md.RenderToString(bytes))
 		}
+	}
+
+	var keys []string
+	for k := range preSort {
+		keys = append(keys, k)
+	}
+	sort.Sort(byFname(keys))
+
+	res := map[string]template.HTML{}
+	for _, k := range keys {
+		val := preSort[k]
+		res[k] = val
 	}
 
 	return res

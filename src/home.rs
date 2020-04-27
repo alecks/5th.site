8977f2e045204dcc1f524e77301da5183aec47c1
Alex P <alexeek@protonmail.com>
2020-04-27 11:32:00 +0100

    🐛 Don't render the example section

--- src/home.rs
+++ src/home.rs
@@ -25,14 +25,18 @@ pub async fn handle(hb: web::Data<Handlebars<'_>>) -> HttpResponse {
 
     let mut html_output = vec![];
     for filename in filenames {
-        // Let's parse the markdown.
-        let content = fs::read_to_string(filename).expect("File in sections dir couldn't be read.");
-        let parser = Parser::new_ext(&*content, options);
+        // TODO: Don't hardcode this.
+        if filename != "sections/example.md" {
+            // Let's parse the markdown.
+            let content =
+                fs::read_to_string(filename).expect("File in sections dir couldn't be read.");
+            let parser = Parser::new_ext(&*content, options);
 
-        // Push to the output vector.
-        let mut parsed_content = String::new();
-        html::push_html(&mut parsed_content, parser);
-        html_output.push(parsed_content);
+            // Push to the output vector.
+            let mut parsed_content = String::new();
+            html::push_html(&mut parsed_content, parser);
+            html_output.push(parsed_content);
+        }
     }
 
     // Render the page with handlebars.
